6f1953d32385b293605c3fed849131806b7956bb
Idyatov Artur <iar@napoleonit.ru>
2021-11-21 07:34:03 +0000

    final

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 visibility_zone = [(200, 100), (1600, 1500)]
 
 blind_zones = [
-        [(0, 1100), (400, 1600)]
+        # [(0, 1100), (400, 1600)]
 ]
 
-probability_threshold = 0.1
+probability_threshold = 0.75
 mask_proba_threshold = 0.1
